65aade018f83df2dbd8a899617bab40d4c65568b
vktornaj <vktornajpro@gmail.com>
2020-04-27 17:06:32 -0500

    Guardar matrices

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
 // Variables Globales
 let matrices = ['A'];
 let instancias = [];
+let iGraficas = [];
+
+// Clases
+class MatrizContenido {
+    nFilas;
+    nColumnas;
+    contenido;
+    numeros;
+
+    constructor(nFilas = 0, nColumnas = 0) {
+        this.nFilas = nFilas;
+        this.nColumnas = nColumnas;
+        this.contenido = [];
+        for (let i = 0; i < nFilas * nColumnas; i++) {
+            this.contenido[i] = 0;
+        }
+        
+    }
+     
+    llenar() {
+        let numeros = document.querySelectorAll('.input');
+        for (let i = 0; i < this.nColumnas * this.nFilas; i++) {
+            this.contenido[i] = numeros[i].value;
+            console.log('Hello');
+        }
+        console.log(`Llenando ${this.contenido}`);
+    }
+    
+}
+
 // Selectores
 const btnAgregar = document.querySelector('#agregar'),
  lista = document.getElementById('lista'),
@@ -54,12 +84,30 @@ function actualizar() {
     }
 }
 
-// Instrucciones
+// Agregar matrices 
 lista.innerHTML += `<p id="matrizA">Matriz ${matrices[0]}</p>`;
+instancias[matrices.length - 1] = new MatrizContenido(inputFilas.value, inputColumnas.value);
+iGraficas.push(document.querySelectorAll('.input')[matrices.length - 1]);
+iGraficas[iGraficas.length - 1].addEventListener('click', () => {
+    const a = iGraficas.length - 1;
+    let contenedor = document.querySelectorAll('.input');
+    for (let i = 0; i < columns * rows; i++) {
+        
+    }
+});
 
 btnAgregar.addEventListener('click', () => {
     matrices.push(String.fromCharCode(matrices[matrices.length - 1].charCodeAt() + 1));
     let s = matrices[matrices.length - 1];
     lista.innerHTML += `<p id="matriz${s}">Matriz ${s}</p>`;
+    instancias[matrices.length - 1] = new MatrizContenido(inputFilas.value, inputColumnas.value);
+    instancias[matrices.length - 2].llenar();
+    let contenedores = document.querySelectorAll('.input');
+    iGraficas.push(document.querySelectorAll('.input')[matrices.length - 1]);
+    for (let i = 0; i < columns * rows; i++) {
+        contenedores[i].value = '';
+    }
 });
+
+// Mostrar matrices
 
